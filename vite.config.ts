528b8646d2bfef6dfcc9597f7b4ad2fe78286608
SeMech <ioan.ikoval@gmail.com>
2024-08-12 14:35:38 +0300

    update builder config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+export default defineConfig(({ mode }) => {
+  const env = mode === 'production' ? loadEnv(mode, process.cwd(), 'FRONT_') : {};
+
+  return {
+    plugins: [react()],
+    base: env.FRONT_BASE_URL || '/',
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    }
   }
 })
