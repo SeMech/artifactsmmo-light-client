90e0d9c5a2249f2dada126d88f33f89844b714ee
SeMech <ioan.ikoval@gmail.com>
2024-08-13 10:12:08 +0300

    fix render cooldown

--- src/helpers/render-cooldown.ts
+++ src/helpers/render-cooldown.ts
@@ -2,6 +2,9 @@ export const renderCooldown = (expireDate: string) => {
   const currentTime = Date.now()
   const expireTime = (new Date(expireDate)).getTime()
 
+  if (currentTime > expireTime)
+    return '-'
+
   const time = Math.abs(expireTime - currentTime)
 
   return Math.ceil(time/1000) + 's'
